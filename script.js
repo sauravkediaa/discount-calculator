7e981c55e96254993208b7969d46b1d1107515ce
sauravkediaa <sauravkediaa@gmail.com>
2024-06-08 14:57:07 +0530

    1st commit for HTML, CSS & JS files

---
+++ script.js
@@ -0,0 +1,38 @@
+function calculate() {
+    const MRP = parseFloat(document.getElementById('mrp').value);
+    const discountPrice = parseFloat(document.getElementById('discountPrice').value);
+
+    if (isNaN(MRP) || isNaN(discountPrice)) {
+        alert("Please enter valid numbers for Buy and Sell prices.");
+        return;
+    }
+
+    const discountPercent = ((discountPrice) / MRP) * 100;
+
+        document.getElementById('result').innerHTML = `
+            <p><b>Discount Percentage: </b> ${discountPercent.toFixed(2)}%</p>
+        `;
+    }
+
+function resetForm() {
+    document.getElementById('MRP').value = '';
+    document.getElementById('discountPrice').value = '';
+    document.getElementById('result').innerHTML = '';
+}
+
+function handleEnterKeyPress(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault(); // Prevent default action of Enter key
+        calculate(); // Call calculate function
+    }
+    if (event.keyCode === 27) {
+        event.preventDefault();
+        resetForm(); // Call resetForm function
+    }
+}
+
+// Event listeners for input fields
+document.getElementById('mrp').addEventListener('keydown', handleEnterKeyPress);
+document.getElementById('discountPrice').addEventListener('keydown', handleEnterKeyPress);
+document.getElementById('calculateBtn').addEventListener('click', calculate);
+document.getElementById('resetBtn').addEventListener('click', resetForm);
